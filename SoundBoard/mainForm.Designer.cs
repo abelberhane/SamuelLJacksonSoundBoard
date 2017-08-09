9d4fb9feefd102bfd883de4f550188c20227f674
Abel <abelberhane@gmail.com>
2017-08-09 15:43:49 -0400

    First commit of the soundboard

---
+++ SoundBoard/mainForm.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace SoundBoard
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.ambulanceButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.askingButton = new System.Windows.Forms.Button();
+            this.attitudeButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.errorButton = new System.Windows.Forms.Button();
+            this.enoughButton = new System.Windows.Forms.Button();
+            this.countryButton = new System.Windows.Forms.Button();
+            this.cantButton = new System.Windows.Forms.Button();
+            this.questionsButton = new System.Windows.Forms.Button();
+            this.kahunaButton = new System.Windows.Forms.Button();
+            this.jobButton = new System.Windows.Forms.Button();
+            this.howButton = new System.Windows.Forms.Button();
+            this.walkButton = new System.Windows.Forms.Button();
+            this.talkingButton = new System.Windows.Forms.Button();
+            this.stfuButton = new System.Windows.Forms.Button();
+            this.retortButton = new System.Windows.Forms.Button();
+            this.youButton = new System.Windows.Forms.Button();
+            this.rememberButton = new System.Windows.Forms.Button();
+            this.fonzieButton = new System.Windows.Forms.Button();
+            this.buttsButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ambulanceButton
+            // 
+            this.ambulanceButton.Location = new System.Drawing.Point(58, 277);
+            this.ambulanceButton.Name = "ambulanceButton";
+            this.ambulanceButton.Size = new System.Drawing.Size(75, 23);
+            this.ambulanceButton.TabIndex = 0;
+            this.ambulanceButton.Text = "Ambulance";
+            this.ambulanceButton.UseVisualStyleBackColor = true;
+            this.ambulanceButton.Click += new System.EventHandler(this.ambulanceButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::SoundBoard.Properties.Resources.SamuelCover;
+            this.pictureBox1.Location = new System.Drawing.Point(58, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(536, 194);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // askingButton
+            // 
+            this.askingButton.Location = new System.Drawing.Point(171, 277);
+            this.askingButton.Name = "askingButton";
+            this.askingButton.Size = new System.Drawing.Size(75, 23);
+            this.askingButton.TabIndex = 2;
+            this.askingButton.Text = "Asking";
+            this.askingButton.UseVisualStyleBackColor = true;
+            this.askingButton.Click += new System.EventHandler(this.askingButton_Click);
+            // 
+            // attitudeButton
+            // 
+            this.attitudeButton.Location = new System.Drawing.Point(289, 277);
+            this.attitudeButton.Name = "attitudeButton";
+            this.attitudeButton.Size = new System.Drawing.Size(75, 23);
+            this.attitudeButton.TabIndex = 3;
+            this.attitudeButton.Text = "Attitude";
+            this.attitudeButton.UseVisualStyleBackColor = true;
+            this.attitudeButton.Click += new System.EventHandler(this.attitudeButton_Click);
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(409, 277);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 4;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // errorButton
+            // 
+            this.errorButton.Location = new System.Drawing.Point(409, 321);
+            this.errorButton.Name = "errorButton";
+            this.errorButton.Size = new System.Drawing.Size(75, 23);
+            this.errorButton.TabIndex = 8;
+            this.errorButton.Text = "Error";
+            this.errorButton.UseVisualStyleBackColor = true;
+            this.errorButton.Click += new System.EventHandler(this.errorButton_Click);
+            // 
+            // enoughButton
+            // 
+            this.enoughButton.Location = new System.Drawing.Point(289, 321);
+            this.enoughButton.Name = "enoughButton";
+            this.enoughButton.Size = new System.Drawing.Size(75, 23);
+            this.enoughButton.TabIndex = 7;
+            this.enoughButton.Text = "Enough";
+            this.enoughButton.UseVisualStyleBackColor = true;
+            this.enoughButton.Click += new System.EventHandler(this.enoughButton_Click);
+            // 
+            // countryButton
+            // 
+            this.countryButton.Location = new System.Drawing.Point(171, 321);
+            this.countryButton.Name = "countryButton";
+            this.countryButton.Size = new System.Drawing.Size(75, 23);
+            this.countryButton.TabIndex = 6;
+            this.countryButton.Text = "Country";
+            this.countryButton.UseVisualStyleBackColor = true;
+            this.countryButton.Click += new System.EventHandler(this.countryButton_Click);
+            // 
+            // cantButton
+            // 
+            this.cantButton.Location = new System.Drawing.Point(58, 321);
+            this.cantButton.Name = "cantButton";
+            this.cantButton.Size = new System.Drawing.Size(75, 23);
+            this.cantButton.TabIndex = 5;
+            this.cantButton.Text = "Cant";
+            this.cantButton.UseVisualStyleBackColor = true;
+            this.cantButton.Click += new System.EventHandler(this.cantButton_Click);
+            // 
+            // questionsButton
+            // 
+            this.questionsButton.Location = new System.Drawing.Point(409, 365);
+            this.questionsButton.Name = "questionsButton";
+            this.questionsButton.Size = new System.Drawing.Size(75, 23);
+            this.questionsButton.TabIndex = 12;
+            this.questionsButton.Text = "Questions";
+            this.questionsButton.UseVisualStyleBackColor = true;
+            this.questionsButton.Click += new System.EventHandler(this.questionsButton_Click);
+            // 
+            // kahunaButton
+            // 
+            this.kahunaButton.Location = new System.Drawing.Point(289, 365);
+            this.kahunaButton.Name = "kahunaButton";
+            this.kahunaButton.Size = new System.Drawing.Size(75, 23);
+            this.kahunaButton.TabIndex = 11;
+            this.kahunaButton.Text = "Kahuna";
+            this.kahunaButton.UseVisualStyleBackColor = true;
+            this.kahunaButton.Click += new System.EventHandler(this.kahunaButton_Click);
+            // 
+            // jobButton
+            // 
+            this.jobButton.Location = new System.Drawing.Point(171, 365);
+            this.jobButton.Name = "jobButton";
+            this.jobButton.Size = new System.Drawing.Size(75, 23);
+            this.jobButton.TabIndex = 10;
+            this.jobButton.Text = "Job";
+            this.jobButton.UseVisualStyleBackColor = true;
+            this.jobButton.Click += new System.EventHandler(this.jobButton_Click);
+            // 
+            // howButton
+            // 
+            this.howButton.Location = new System.Drawing.Point(58, 365);
+            this.howButton.Name = "howButton";
+            this.howButton.Size = new System.Drawing.Size(75, 23);
+            this.howButton.TabIndex = 9;
+            this.howButton.Text = "How";
+            this.howButton.UseVisualStyleBackColor = true;
+            this.howButton.Click += new System.EventHandler(this.howButton_Click);
+            // 
+            // walkButton
+            // 
+            this.walkButton.Location = new System.Drawing.Point(409, 406);
+            this.walkButton.Name = "walkButton";
+            this.walkButton.Size = new System.Drawing.Size(75, 23);
+            this.walkButton.TabIndex = 16;
+            this.walkButton.Text = "Walk";
+            this.walkButton.UseVisualStyleBackColor = true;
+            this.walkButton.Click += new System.EventHandler(this.walkButton_Click);
+            // 
+            // talkingButton
+            // 
+            this.talkingButton.Location = new System.Drawing.Point(289, 406);
+            this.talkingButton.Name = "talkingButton";
+            this.talkingButton.Size = new System.Drawing.Size(75, 23);
+            this.talkingButton.TabIndex = 15;
+            this.talkingButton.Text = "Talking";
+            this.talkingButton.UseVisualStyleBackColor = true;
+            this.talkingButton.Click += new System.EventHandler(this.talkingButton_Click);
+            // 
+            // stfuButton
+            // 
+            this.stfuButton.Location = new System.Drawing.Point(171, 406);
+            this.stfuButton.Name = "stfuButton";
+            this.stfuButton.Size = new System.Drawing.Size(75, 23);
+            this.stfuButton.TabIndex = 14;
+            this.stfuButton.Text = "STFU";
+            this.stfuButton.UseVisualStyleBackColor = true;
+            this.stfuButton.Click += new System.EventHandler(this.stfuButton_Click);
+            // 
+            // retortButton
+            // 
+            this.retortButton.Location = new System.Drawing.Point(58, 406);
+            this.retortButton.Name = "retortButton";
+            this.retortButton.Size = new System.Drawing.Size(75, 23);
+            this.retortButton.TabIndex = 13;
+            this.retortButton.Text = "Retort";
+            this.retortButton.UseVisualStyleBackColor = true;
+            this.retortButton.Click += new System.EventHandler(this.retortButton_Click);
+            // 
+            // youButton
+            // 
+            this.youButton.Location = new System.Drawing.Point(519, 406);
+            this.youButton.Name = "youButton";
+            this.youButton.Size = new System.Drawing.Size(75, 23);
+            this.youButton.TabIndex = 20;
+            this.youButton.Text = "You";
+            this.youButton.UseVisualStyleBackColor = true;
+            this.youButton.Click += new System.EventHandler(this.youButton_Click);
+            // 
+            // rememberButton
+            // 
+            this.rememberButton.Location = new System.Drawing.Point(519, 365);
+            this.rememberButton.Name = "rememberButton";
+            this.rememberButton.Size = new System.Drawing.Size(75, 23);
+            this.rememberButton.TabIndex = 19;
+            this.rememberButton.Text = "Remember";
+            this.rememberButton.UseVisualStyleBackColor = true;
+            this.rememberButton.Click += new System.EventHandler(this.rememberButton_Click);
+            // 
+            // fonzieButton
+            // 
+            this.fonzieButton.Location = new System.Drawing.Point(519, 321);
+            this.fonzieButton.Name = "fonzieButton";
+            this.fonzieButton.Size = new System.Drawing.Size(75, 23);
+            this.fonzieButton.TabIndex = 18;
+            this.fonzieButton.Text = "Fonzie";
+            this.fonzieButton.UseVisualStyleBackColor = true;
+            this.fonzieButton.Click += new System.EventHandler(this.fonzieButton_Click);
+            // 
+            // buttsButton
+            // 
+            this.buttsButton.Location = new System.Drawing.Point(519, 277);
+            this.buttsButton.Name = "buttsButton";
+            this.buttsButton.Size = new System.Drawing.Size(75, 23);
+            this.buttsButton.TabIndex = 17;
+            this.buttsButton.Text = "Butts";
+            this.buttsButton.UseVisualStyleBackColor = true;
+            this.buttsButton.Click += new System.EventHandler(this.buttsButton_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SoundBoard.Properties.Resources.Wallpaper;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(657, 449);
+            this.Controls.Add(this.youButton);
+            this.Controls.Add(this.rememberButton);
+            this.Controls.Add(this.fonzieButton);
+            this.Controls.Add(this.buttsButton);
+            this.Controls.Add(this.walkButton);
+            this.Controls.Add(this.talkingButton);
+            this.Controls.Add(this.stfuButton);
+            this.Controls.Add(this.retortButton);
+            this.Controls.Add(this.questionsButton);
+            this.Controls.Add(this.kahunaButton);
+            this.Controls.Add(this.jobButton);
+            this.Controls.Add(this.howButton);
+            this.Controls.Add(this.errorButton);
+            this.Controls.Add(this.enoughButton);
+            this.Controls.Add(this.countryButton);
+            this.Controls.Add(this.cantButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.attitudeButton);
+            this.Controls.Add(this.askingButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ambulanceButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.Text = "SoundBoard";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ambulanceButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button askingButton;
+        private System.Windows.Forms.Button attitudeButton;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button errorButton;
+        private System.Windows.Forms.Button enoughButton;
+        private System.Windows.Forms.Button countryButton;
+        private System.Windows.Forms.Button cantButton;
+        private System.Windows.Forms.Button questionsButton;
+        private System.Windows.Forms.Button kahunaButton;
+        private System.Windows.Forms.Button jobButton;
+        private System.Windows.Forms.Button howButton;
+        private System.Windows.Forms.Button walkButton;
+        private System.Windows.Forms.Button talkingButton;
+        private System.Windows.Forms.Button stfuButton;
+        private System.Windows.Forms.Button retortButton;
+        private System.Windows.Forms.Button youButton;
+        private System.Windows.Forms.Button rememberButton;
+        private System.Windows.Forms.Button fonzieButton;
+        private System.Windows.Forms.Button buttsButton;
+    }
+}
+
---
+++ SoundBoard/mainForm.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//
+// This was much simpler than I expected. I needed to add Using System.Media to use the Sound player class. 
+//Pass the specific location of where the audio file was located and then instantiated the play property on each click. 
+//
+
+namespace SoundBoard
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+       
+        private void ambulanceButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Ambulance);
+            sp.Play();
+        }
+
+        private void askingButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Asking);
+            sp.Play();
+        }
+
+        private void attitudeButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Attitude);
+            sp.Play();
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Back);
+            sp.Play();
+        }
+
+        private void buttsButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Butts);
+            sp.Play();
+        }
+
+        private void cantButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Cant);
+            sp.Play();
+        }
+
+        private void countryButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Country);
+            sp.Play();
+        }
+
+        private void enoughButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Enough);
+            sp.Play();
+        }
+
+        private void errorButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Error);
+            sp.Play();
+        }
+
+        private void fonzieButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Fonzie);
+            sp.Play();
+        }
+
+        private void howButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.How);
+            sp.Play();
+        }
+
+        private void jobButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Job);
+            sp.Play();
+        }
+
+        private void kahunaButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Kahuna);
+            sp.Play();
+        }
+
+        private void questionsButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Questions);
+            sp.Play();
+        }
+
+        private void rememberButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Remember);
+            sp.Play();
+        }
+
+        private void retortButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Retort);
+            sp.Play();
+        }
+
+        private void stfuButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.STFU);
+            sp.Play();
+        }
+
+        private void talkingButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Talking);
+            sp.Play();
+        }
+
+        private void walkButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.Walk);
+            sp.Play();
+        }
+
+        private void youButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(SoundBoard.Properties.Resources.You);
+            sp.Play();
+        }
+    }
+}
